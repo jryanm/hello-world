ec89b5ba85f0ce8c776cca5b8fd7d3541db794e5
jryanm <103079145+jryanm@users.noreply.github.com>
2022-04-08 20:54:09 -0400

    Add files via upload

---
+++ HelloWorld.cpp
@@ -0,0 +1,15 @@
+/****************************************************************************
+// Joshua Martin          -         CSC 201         -          April 10, 2022
+//
+// Program: Hello World Github Project
+//
+// This program prints "Hello World!" to the screen. But honestly the real
+// point of this is learning how to use Github, so that's how you're probably
+// reading this now.
+****************************************************************************/
+#include <iostream>  // For std::cout and std::endl
+
+int main(){
+    std::cout << "Hello World!" << std::endl;
+    return 0;
+}
